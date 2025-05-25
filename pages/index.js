6b9e6f236440bf9e024aade096985fe689c046a3
cybev1 <cybev.io@gmail.com>
2025-05-24 23:21:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ export default function Home() {
   return (
     <div>
       <Hero onGetStarted={() => router.push('/login')} />
+      <Slides />
       <PromoVideo />
       <Features />
       <Testimonials />
