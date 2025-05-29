53176f18ec2adc04e5624bc89c796bc9f082adba
cybev1 <cybev.io@gmail.com>
2025-05-28 20:12:46 -0700

    Add files via upload

---
+++ components/blog/BlogEditor.js
@@ -0,0 +1,11 @@
+// Enhanced blog editor component (placeholder)
+import { useState } from 'react';
+
+export default function BlogEditor() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">AI Blog Post Editor</h1>
+      {/* AI form, Upload, SEO, Mint, Boost, etc. will be inserted here */}
+    </div>
+  );
+}
