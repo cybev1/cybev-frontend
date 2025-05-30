2e50116dc8194e4be096e210818d81c805d89c65
cybev1 <cybev.io@gmail.com>
2025-05-30 21:59:03 +0000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 
 import '../styles/globals.css';
 import { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState('light');
@@ -11,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <div className={theme === 'dark' ? 'dark' : ''}>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Component {...pageProps} theme={theme} setTheme={setTheme} />
     </div>
   );
