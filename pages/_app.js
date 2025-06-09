fab11b773edb665a3bf27e59aa92ce87e9c03800
cybev1 <cybev.io@gmail.com>
2025-06-09 19:31:09 +0000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-
-import '../styles/globals.css'
-import Navbar from '../components/Navbar'
+import '../styles/globals.css';
+import Navbar from '../components/Navbar';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,8 +10,9 @@ function MyApp({ Component, pageProps }) {
       <main className="pt-20 px-4 md:px-12 bg-gray-50 min-h-screen">
         <Component {...pageProps} />
       </main>
+      <ToastContainer position="top-right" autoClose={3000} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
