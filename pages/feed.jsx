25448b0586ec8b81bf1ce00ee9fee172500ffb0c
cybev1 <cybev.io@gmail.com>
2025-06-14 06:09:20 +0000

    Add files via upload

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -4,72 +4,39 @@ import axios from 'axios';
 import LeftNav from '../components/social/LeftNav';
 import GreetingWeatherStrip from '../components/social/GreetingWeatherStrip';
 import StoriesCarousel from '../components/social/StoriesCarousel';
+import LiveNowStrip from '../components/social/LiveNowStrip';
 import PinPostCard from '../components/social/PinPostCard';
-import SuperBloggerCard from '../components/social/SuperBloggerCard';
+import SuperBloggerStrip from '../components/social/SuperBloggerStrip';
 import PostComposer from '../components/social/PostComposer';
 import PostCard from '../components/social/PostCard';
 import AdCard from '../components/social/AdCard';
 import SuggestionCard from '../components/social/SuggestionCard';
-import LiveNowStrip from '../components/social/LiveNowStrip';
 import NewsTicker from '../components/social/NewsTicker';
-import FollowersList from '../components/social/FollowersList';
-import SuggestedFollowers from '../components/social/SuggestedFollowers';
-import GroupsList from '../components/social/GroupsList';
-import SuggestedEvents from '../components/social/SuggestedEvents';
-import CyBevBot from '../components/social/CyBevBot';
 
 const fetcher = url => axios.get(url).then(res => res.data);
 
-export default function Feed() {
-  const { data: posts, error } = useSWR('/api/posts/feed', fetcher);
-
-  if (error) return <div>Error loading feed</div>;
-  if (!posts) return <div>Loading...</div>;
+export default function FeedPage() {
+  const { data: feed } = useSWR('/api/posts/feed', fetcher);
 
   return (
     <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
-      {/* Left Navigation */}
-      <div className="hidden lg:block w-60 border-r bg-white dark:bg-gray-800">
-        <LeftNav />
-      </div>
-
-      {/* Center Feed */}
-      <div className="flex-1 overflow-auto p-4">
-        <GreetingWeatherStrip
-          greeting="Good morning Prince"
-          motivational="Today is a great day and you are winning!"
-          weather={{ temp: 72, icon: '☀️' }}
-        />
+      <LeftNav />
+      <div className="flex-1 overflow-auto p-4 space-y-4">
+        <GreetingWeatherStrip />
         <StoriesCarousel />
+        <LiveNowStrip />
         <PinPostCard />
-        <SuperBloggerCard />
+        <SuperBloggerStrip />
         <PostComposer />
-
-        {posts.map((post, idx) => (
-          <React.Fragment key={post.id}>
-            {idx === 2 && <AdCard />}
-            {idx === 5 && <LiveNowStrip />}
-            {idx === 8 && <NewsTicker />}
-            {idx === 10 && <SuggestionCard />}
-            <PostCard 
-              author={post.author} 
-              timestamp={post.timestamp} 
-              content={post.content} 
-              earnings={post.earnings} 
-            />
-          </React.Fragment>
-        ))}
+        {feed ? feed.map((post, idx) => {
+          if (idx > 0 && idx % 3 === 0) return <AdCard key={'ad'+idx} />;
+          if (idx > 0 && idx % 10 === 0) return <SuggestionCard key={'sug'+idx} />;
+          return <PostCard key={post.id} post={post} />;
+        }) : <div>Loading feed...</div>}
+        <NewsTicker />
       </div>
-
-      {/* Right Sidebar */}
-      <div className="hidden xl:block w-80 border-l p-4 bg-white dark:bg-gray-800 overflow-auto">
-        <FollowersList />
-        <SuggestedFollowers />
-        <GroupsList />
-        <SuggestedEvents />
+      <div className="w-80 p-4 hidden lg:block">
+        {/* Right Hub placeholder */}
       </div>
-
-      <CyBevBot />
     </div>
-  );
 }
