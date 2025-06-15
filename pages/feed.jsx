e98176f8e6da4e7e22b3f3eef040697efa013b03
cybev1 <cybev.io@gmail.com>
2025-06-15 04:10:18 +0000

    Add files via upload

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,44 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LeftNav from '../components/social/LeftNav';
 import GreetingWeatherStrip from '../components/social/GreetingWeatherStrip';
 import StoriesCarousel from '../components/social/StoriesCarousel';
+import LiveNowStrip from '../components/social/LiveNowStrip';
+import NewsTicker from '../components/social/NewsTicker';
 import PinPostCard from '../components/social/PinPostCard';
 import SuperBloggerBadge from '../components/social/SuperBloggerBadge';
 import PostComposer from '../components/social/PostComposer';
-import LiveNowStrip from '../components/social/LiveNowStrip';
-import NewsTicker from '../components/social/NewsTicker';
 import PostCard from '../components/social/PostCard';
 import AdCard from '../components/social/AdCard';
 import SuggestionCard from '../components/social/SuggestionCard';
 import RightHub from '../components/social/RightHub';
 
 export default function Feed() {
-  const [stories] = useState([{}, {}, {}, {}]);
-  const [pinnedPost] = useState({ title: 'This is a pinned post' });
-  const [liveStream] = useState({ title: 'Admin is live streaming!' });
-  const [headlines] = useState(['Headline 1', 'Headline 2', 'Headline 3']);
-  const [feed] = useState([
-    { id: 1, author: 'Alice', time: '2h ago', content: 'Hello world!', likes: 10, comments: 2, shares: 1, views: 15 },
-    { id: 2, author: 'Bob', time: '3h ago', content: 'Another post', likes: 5, comments: 1, shares: 0, views: 10 },
-  ]);
-  const [rightData] = useState({});
+  const [feed] = useState([{ id: 1, title: 'Hello World' }]);
+  const [liveStream] = useState(true);
 
   return (
-    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex">
       <LeftNav />
-      <main className="flex-1 overflow-auto p-4">
+      <div className="flex-1 p-4 space-y-4">
         <GreetingWeatherStrip />
-        <StoriesCarousel stories={stories} />
-        <LiveNowStrip stream={liveStream} />
-        <NewsTicker headlines={headlines} />
-        <PinPostCard post={pinnedPost} />
+        <StoriesCarousel />
+        {liveStream && <LiveNowStrip />}
+        <NewsTicker />
+        <PinPostCard />
         <SuperBloggerBadge />
         <PostComposer />
-        {feed.map((post) => <PostCard key={post.id} post={post} />)}
+        {feed.map(post => <PostCard key={post.id} post={post} />)}
         <AdCard />
         <SuggestionCard suggestion={{ title: 'People You May Know' }} />
-      </main>
-      <RightHub data={rightData} />
+      </div>
+      <RightHub />
     </div>
   );
 }
