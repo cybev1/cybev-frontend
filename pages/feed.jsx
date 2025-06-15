ae50613bec290e366b19857c743cc0e9c72961e9
cybev1 <cybev.io@gmail.com>
2025-06-15 04:20:30 +0000

    Add files via upload

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,37 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LeftNav from '../components/social/LeftNav';
 import GreetingWeatherStrip from '../components/social/GreetingWeatherStrip';
 import StoriesCarousel from '../components/social/StoriesCarousel';
 import LiveNowStrip from '../components/social/LiveNowStrip';
-import NewsTicker from '../components/social/NewsTicker';
-import PinPostCard from '../components/social/PinPostCard';
-import SuperBloggerBadge from '../components/social/SuperBloggerBadge';
 import PostComposer from '../components/social/PostComposer';
 import PostCard from '../components/social/PostCard';
-import AdCard from '../components/social/AdCard';
-import SuggestionCard from '../components/social/SuggestionCard';
 import RightHub from '../components/social/RightHub';
 
 export default function Feed() {
-  const [feed] = useState([{ id: 1, title: 'Hello World' }]);
-  const [liveStream] = useState(true);
-
   return (
-    <div className="flex">
-      <LeftNav />
-      <div className="flex-1 p-4 space-y-4">
+    <div className="flex flex-col md:flex-row h-screen">
+      <aside className="hidden md:block w-full md:w-1/4 border-r p-4"><LeftNav /></aside>
+      <main className="flex-1 overflow-auto p-4 space-y-4">
         <GreetingWeatherStrip />
         <StoriesCarousel />
-        {liveStream && <LiveNowStrip />}
-        <NewsTicker />
-        <PinPostCard />
-        <SuperBloggerBadge />
+        <LiveNowStrip />
         <PostComposer />
-        {feed.map(post => <PostCard key={post.id} post={post} />)}
-        <AdCard />
-        <SuggestionCard suggestion={{ title: 'People You May Know' }} />
-      </div>
-      <RightHub />
+        {[1,2,3].map(i => <PostCard key={i} />)}
+      </main>
+      <aside className="hidden lg:block w-full lg:w-1/4 border-l p-4"><RightHub /></aside>
     </div>
   );
 }
