1b87b846c00b56cd45edc2a661df631817867545
cybev1 <cybev.io@gmail.com>
2025-06-15 03:53:05 +0000

    Delete components/social/CyBevBot.jsx

--- components/social/CyBevBot.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CyBevBot() {
-  return (
-    <div className="fixed bottom-4 right-4 bg-white dark:bg-gray-800 p-3 rounded-full shadow-lg cursor-pointer">
-      🤖
-    </div>
-  );
-}
