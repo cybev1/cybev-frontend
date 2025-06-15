d4712c4cdbc6ba9ad034e6ccf6b429e1cb1144c0
cybev1 <cybev.io@gmail.com>
2025-06-15 09:57:16 +0000

    Add files via upload

--- components/social/SuperBloggerCard.jsx
+++ components/social/SuperBloggerCard.jsx
@@ -1,10 +1,14 @@
+
 import React from 'react';
 
 export default function SuperBloggerCard() {
   return (
-    <div className="p-4 bg-green-100 dark:bg-green-900 rounded-lg shadow mb-4 flex items-center">
-      <span className="badge bg-green-600 text-white mr-2">Super Blogger</span>
-      {/* TODO: Display super blogger info */}
+    <div className="bg-blue-100 dark:bg-blue-900 p-3 rounded-lg shadow mb-4 flex items-center space-x-3">
+      <img src="/default-avatar.png" alt="Jane Smith" className="w-12 h-12 rounded-full" />
+      <div>
+        <div className="font-semibold text-blue-800 dark:text-white">Jane Smith 🔥</div>
+        <div className="text-sm text-blue-700 dark:text-blue-300">Super Blogger</div>
+      </div>
     </div>
   );
 }
