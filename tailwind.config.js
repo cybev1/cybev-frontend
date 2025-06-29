928c240d09d656d9a24326f0873a014ee2235341
cybev1 <cybev.io@gmail.com>
2025-06-29 17:42:29 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      animation: {
-        pulse: "pulse 4s infinite",
-        ping: "ping 6s infinite",
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
