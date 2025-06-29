efa2be6d5333360c6c104ef9ea35fea1a98fb0d9
cybev1 <cybev.io@gmail.com>
2025-06-29 17:14:40 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 1s ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      }
+    },
   },
   plugins: [],
 }
