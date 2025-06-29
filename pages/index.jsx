d00b6c0567b87653c07e4c4661a2ed66c0c27ce0
cybev1 <cybev.io@gmail.com>
2025-06-29 22:34:14 +0000

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
@@ -6,11 +5,11 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-colors duration-300">
       <Navbar />
       <Hero />
       <Features />
       <Footer />
-    </>
+    </div>
   );
 }
