b230a65105868e52493b2eecb629f36a3df4be97
cybev1 <cybev.io@gmail.com>
2025-06-29 19:56:31 +0000

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
-
+import Head from 'next/head';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className="bg-gray-50 dark:bg-black text-gray-900 dark:text-white">
-      <Hero />
-      <Features />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>CYBEV – Create. Earn. Mint. Grow.</title>
+        <meta name="description" content="Your Gen-Z powered Web3 platform for creators." />
+      </Head>
+      <div className="bg-gradient-to-br from-purple-900 via-black to-pink-900 text-white">
+        <Hero />
+        <Features />
+        <Footer />
+      </div>
+    </>
   );
 }
