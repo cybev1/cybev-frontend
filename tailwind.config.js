7f721990890ed7197128980eec880d0d94434594
cybev1 <cybev.io@gmail.com>
2025-06-29 17:58:52 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#570DF8',
-        secondary: '#F000B8',
-        accent: '#37CDBE',
-        neutral: '#3D4451',
-        base: '#FFFFFF'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
