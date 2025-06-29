3da94aebb1c5aba3d5d4050c30d03cd5f0328f8c
cybev1 <cybev.io@gmail.com>
2025-06-29 15:44:13 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        primary: '#5F3EED',
-        secondary: '#A689FC',
-        accent: '#D2BFFF',
-        muted: '#F5F3FF',
-      },
-      boxShadow: {
-        glow: '0 0 8px rgba(95, 62, 237, 0.5)',
-      },
-    },
+    extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [],
 }
