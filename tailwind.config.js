0b45f00b25c651939efef849660eab778b5e1540
cybev1 <cybev.io@gmail.com>
2025-06-29 16:36:18 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,42 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      },
       colors: {
-        'cybev-dark': '#0b0c2a',
-        'cybev-purple': '#6610f2',
+        'cybev-primary': '#8b5cf6',
+        'cybev-accent': '#ec4899',
       },
+      animation: {
+        fade: 'fadeIn 2s ease-in forwards',
+        bounceIn: 'bounceIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        bounceIn: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(0.3)'
+          },
+          '50%': {
+            opacity: 1,
+            transform: 'scale(1.05)'
+          },
+          '70%': {
+            transform: 'scale(0.9)'
+          },
+          '100%': {
+            transform: 'scale(1)'
+          }
+        }
+      }
     },
   },
   plugins: [],
