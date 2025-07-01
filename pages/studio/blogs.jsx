c391af345c797debc557f70b8789c269f8aa5919
cybev1 <cybev.io@gmail.com>
2025-07-01 21:08:09 +0000

    Add files via upload

---
+++ pages/studio/blogs.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from '@/components/DashboardLayout';
+
+export default function MyBlogs() {
+  return (
+    <DashboardLayout title="My Blogs">
+      <p className="p-4">Your blogs will appear here.</p>
+    </DashboardLayout>
+  );
+}
---
+++ pages/studio/create.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from '@/components/DashboardLayout';
+
+export default function CreatePost() {
+  return (
+    <DashboardLayout title="Create Post">
+      <p className="p-4">Post creation interface coming soon.</p>
+    </DashboardLayout>
+  );
+}
