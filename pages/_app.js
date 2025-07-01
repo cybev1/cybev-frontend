664507f2e522ea6052ef0adfbc7311a3b58308cc
cybev1 <cybev.io@gmail.com>
2025-07-01 04:52:38 +0000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <main className="pt-20 px-4 md:px-12 bg-gray-50 dark:bg-black min-h-screen">
+      <main className="pt-20 px-4 md:px-12 bg-gray-50 min-h-screen dark:bg-black dark:text-white">
         <Component {...pageProps} />
       </main>
       <ToastContainer position="top-right" autoClose={3000} />
