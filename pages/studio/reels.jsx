e06d722d716ef3c0921940e1a3239d75404121e9
cybev1 <cybev.io@gmail.com>
2025-07-01 21:53:50 +0000

    Add files via upload

---
+++ pages/studio/reels.jsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '@/components/DashboardLayout';
+import ReelsPreview from '@/components/ReelsPreview';
+
+export default function ReelsPage() {
+  return (
+    <DashboardLayout title="Reels">
+      <ReelsPreview />
+    </DashboardLayout>
+  );
+}
