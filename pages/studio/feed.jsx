31bbd2bcb559f93230e3c9fe5e5325c27a4d3ca5
cybev1 <cybev.io@gmail.com>
2025-07-02 15:15:00 +0000

    Add files via upload

--- pages/studio/feed.jsx
+++ pages/studio/feed.jsx
@@ -1,24 +1,36 @@
 
 import React from 'react';
-import PostCard from '../components/PostCard';
-import RightSidebarCards from '../components/RightSidebarCards';
+import PostCard from '@/components/PostCard';
+import RightSidebarCards from '@/components/RightSidebarCards';
 
-const dummyPosts = [
-  { id: 1, title: "Hello CYBEV!", content: "Welcome to the new platform!" },
-  { id: 2, title: "Web3 Revolution", content: "Mint, stake, and earn!" }
+const samplePosts = [
+  {
+    id: '1',
+    userAvatar: 'https://i.pravatar.cc/150?img=1',
+    username: 'web3fan',
+    timeAgo: '2h ago',
+    content: 'Just minted my first NFT on CYBEV! Loving the experience 🚀',
+    image: 'https://source.unsplash.com/random/400x200?crypto'
+  },
+  {
+    id: '2',
+    userAvatar: 'https://i.pravatar.cc/150?img=2',
+    username: 'blockbuilder',
+    timeAgo: '5h ago',
+    content: 'Exploring decentralized social media on CYBEV.',
+    image: null
+  }
 ];
 
 export default function Feed() {
   return (
-    <div className="flex">
-      <div className="w-2/3 p-6">
-        {dummyPosts.map(post => (
+    <div className="flex flex-col lg:flex-row gap-4">
+      <main className="lg:w-3/4 p-4">
+        {samplePosts.map(post => (
           <PostCard key={post.id} post={post} />
         ))}
-      </div>
-      <div className="w-1/3 p-6">
-        <RightSidebarCards />
-      </div>
+      </main>
+      <RightSidebarCards />
     </div>
   );
 }
