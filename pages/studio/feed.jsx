06e8b4e4360c0297c9671614fae13b36f50b4c86
cybev1 <cybev.io@gmail.com>
2025-07-02 15:06:08 +0000

    Add files via upload

--- pages/studio/feed.jsx
+++ pages/studio/feed.jsx
@@ -1,27 +1,22 @@
+
 import React from 'react';
-import PostCard from '@/components/PostCard';
-import RightSidebarCards from '@/components/RightSidebarCards';
+import PostCard from '../components/PostCard';
+import RightSidebarCards from '../components/RightSidebarCards';
+
+const dummyPosts = [
+  { id: 1, title: "Hello CYBEV!", content: "Welcome to the new platform!" },
+  { id: 2, title: "Web3 Revolution", content: "Mint, stake, and earn!" }
+];
 
 export default function Feed() {
   return (
-    <div className="flex w-full px-4 md:px-12 pt-20 gap-6 bg-gray-50 dark:bg-black min-h-screen">
-      <div className="flex flex-col w-full max-w-2xl mx-auto space-y-6">
-        {[1, 2, 3].map((id) => (
-          <PostCard
-            key={id}
-            post={{
-              id,
-              username: "cybev_user",
-              content: "This is a sample post on CYBEV!",
-              image: "/sample.jpg",
-              views: 120 + id,
-              reactions: 10 * id,
-              earnings: 1.2 * id,
-            }}
-          />
+    <div className="flex">
+      <div className="w-2/3 p-6">
+        {dummyPosts.map(post => (
+          <PostCard key={post.id} post={post} />
         ))}
       </div>
-      <div className="hidden xl:block w-80">
+      <div className="w-1/3 p-6">
         <RightSidebarCards />
       </div>
     </div>
