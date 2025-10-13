c6f92ba71df0b370ac3635a42c7e02f654dbfa1d
cybev1 <cybev.io@gmail.com>
2025-10-13 18:28:29 +0000

    Update mint-test.jsx

--- pages/studio/mint-test.jsx
+++ pages/studio/mint-test.jsx
@@ -1,4 +1,5 @@
-import MintNFTCard from '@/components/MintNFTCard';
+import MintNFTCard from '../../components/MintNFTCard';
+
 export default function MintTestPage() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-black py-10 px-4">
