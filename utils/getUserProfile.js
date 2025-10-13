9362b8ebc38c3b19a756dd2d573abfb8b98e962e
cybev1 <cybev.io@gmail.com>
2025-10-13 22:44:59 +0000

    Delete utils directory

--- utils/getUserProfile.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default async function getUserProfile() {
-  const token = localStorage.getItem('cybev_token');
-  const res = await axios.get('/api/auth/me', {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return res.data;
-}
