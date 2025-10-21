d098fffb2c8c5b0e6baf4ba5a1c000440e83a5ca
cybev1 <cybev.io@gmail.com>
2025-10-21 09:46:38 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import '@/styles/globals.css';
+import 'react-quill/dist/quill.snow.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <ErrorBoundary>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </ErrorBoundary>
+  );
 }
-
-export default MyApp
