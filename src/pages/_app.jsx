8678c35b24621ab73091bfc09cf8888a19cedfe2
cybev1 <cybev.io@gmail.com>
2025-10-22 20:56:02 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,48 +1,12 @@
-import Script from 'next/script';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { pageview, GA_TRACKING_ID } from '../lib/analytics';
+// src/pages/_app.jsx
+import '../styles/globals.css'; // adjust if your globals path differs
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
-export default function GoogleAnalytics() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  // Only load GA in production
-  if (process.env.NODE_ENV !== 'production' || !GA_TRACKING_ID) {
-    return null;
-  }
-
+export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-              send_page_view: true
-            });
-          `,
-        }}
-      />
+      <GoogleAnalytics />
+      <Component {...pageProps} />
     </>
   );
 }
