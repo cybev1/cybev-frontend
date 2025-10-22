110e9c67adacde2d6409a174bd713c9a209aa574
cybev1 <cybev.io@gmail.com>
2025-10-22 22:22:18 +0000

    Delete src/pages/studio directory

--- src/pages/studio/dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '../../components/DashboardLayout';
-
-export default function Dashboard() {
-  return (
-    <DashboardLayout title="Welcome to CYBEV Dashboard">
-      <div className="p-4 text-lg">🚀 CYBEV is live. Use the sidebar to navigate your Creator Studio.</div>
-    </DashboardLayout>
-  );
-}
