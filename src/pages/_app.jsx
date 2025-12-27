122062636a176dce76986ea21d5564403345673c
cybev1 <cybev.io@gmail.com>
2025-12-27 15:06:02 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,22 +2,25 @@ import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { SocketProvider } from '@/context/SocketContext';
+import { Web3Provider } from '@/context/Web3Context';
 
 export default function App({ Component, pageProps }) {
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </SocketProvider>
+    <Web3Provider>
+      <SocketProvider>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </SocketProvider>
+    </Web3Provider>
   );
 }
